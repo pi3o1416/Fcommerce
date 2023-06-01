09297e97b17037fdcf6302dd75a6da8a8562e37c
= <mmonir.m.29@gmail.com>
2023-06-01 13:42:55 +0600

    Update custom renderer.

--- core/renderers.py
+++ core/renderers.py
@@ -4,14 +4,22 @@ from rest_framework.renderers import JSONRenderer
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         custom_data = {
-            "status": None,
-            "data": None,
-            "error": None,
+            'status': None,
+            'data': None,
+            'error': None,
+        }
+        error_message = {
+            'message': None,
+            'type': None,
+            'detail': None,
         }
         response = renderer_context.get("response")
         custom_data["status"] = response.status_code
         if response.status_code >= 400:
-            custom_data["error"] = data
+            error_message['detail'] = data['detail']
+            error_message['type'] = data['type']
+            error_message['message'] = data['message']
+            custom_data['error'] = error_message
         else:
-            custom_data["data"] = data
+            custom_data['data'] = data
         return super().render(custom_data, accepted_media_type, renderer_context)
