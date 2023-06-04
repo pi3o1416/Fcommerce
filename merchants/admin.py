168fbb9a4c8a2e51c041c7f890a31fe016429c3c
= <mmonir.m.29@gmail.com>
2023-06-04 15:24:33 +0600

    Add Merchant model to django admin panel.

--- merchants/admin.py
+++ merchants/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Merchant
+
+
+@admin.register(Merchant)
+class MerchantAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Merchant._meta.fields]
