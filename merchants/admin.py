f4d309d35f232a9554bec56cf8fb5312841d7273
= <mmonir.m.29@gmail.com>
2023-06-06 13:38:53 +0600

    Remove integrate_facebook and publish_shop from merchant add form.

--- merchants/admin.py
+++ merchants/admin.py
@@ -6,3 +6,7 @@ from .models import Merchant
 @admin.register(Merchant)
 class MerchantAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Merchant._meta.fields]
+
+    def add_view(self, *args, **kwargs):
+        self.exclude = ('integrate_facebook', 'publish_shop')
+        return super(MerchantAdmin, self).add_view(*args, **kwargs)
