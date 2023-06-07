9d546cbc3b4de67723853a96441f179c31a5c4ed
= <mmonir.m.29@gmail.com>
2023-06-07 17:42:15 +0600

    Update custom renderer. Add message field, remove complex error response.

--- core/renderers.py
+++ core/renderers.py
@@ -4,22 +4,19 @@ from rest_framework.renderers import JSONRenderer
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         custom_data = {
+            'message': None,
             'status': None,
             'data': None,
             'error': None,
-        }
-        error_message = {
-            'message': None,
-            'type': None,
-            'detail': None,
+            'error-type': None,
         }
         response = renderer_context.get("response")
         custom_data["status"] = response.status_code
         if response.status_code >= 400:
-            error_message['detail'] = data['detail']
-            error_message['type'] = data['type']
-            error_message['message'] = data['message']
-            custom_data['error'] = error_message
+            custom_data['message'] = data['message']
+            custom_data['error'] = data['detail']
+            custom_data['error-type'] = data['error_type']
         else:
-            custom_data['data'] = data
+            custom_data['message'] = data['message']
+            custom_data['data'] = data['detail']
         return super().render(custom_data, accepted_media_type, renderer_context)
