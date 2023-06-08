7dcfb3c83f9be630ab49520a5dd4924aca588532
= <mmonir.m.29@gmail.com>
2023-06-08 18:25:07 +0600

    Add merchants app url and drf-specticular urls to project urls.

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('merchants/', include('merchants.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
+    path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger",),
+
 ]
