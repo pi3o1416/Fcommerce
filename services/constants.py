10bc55b659c3fdd9cbe1e1ee3c4456fb9763bf0b
= <mmonir.m.29@gmail.com>
2023-06-14 18:14:14 +0600

    Add INVALID_CREDENTIAL and INVALID_REFRESH_TOKEN constants.

--- services/constants.py
+++ services/constants.py
@@ -5,3 +5,5 @@ from enum import Enum
 class ErrorTypes(Enum):
     FORM_FIELD_ERROR = 'form_field_error'
     OBJECT_DOES_NOT_EXIST = 'object_not_found'
+    INVALID_CREDENTIAL = 'invalid_credential'
+    INVALID_REFRESH_TOKEN = 'invalid_refresh_token'
