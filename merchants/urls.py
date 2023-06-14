d596ae8afb27ed809f72970c0a4ee271b366ed49
= <mmonir.m.29@gmail.com>
2023-06-14 18:14:47 +0600

    Add auth views to urlpatterns.

--- merchants/urls.py
+++ merchants/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path, include
-from .views import MerchantModelViewSet
+from .views import MerchantModelViewSet, MyTokenObtainPairView, MyTokenRefreshView, LogoutView
 from .routers import CustomRouter
 
 
@@ -9,5 +9,8 @@ router.register('', MerchantModelViewSet)
 
 app_name = 'merchant'
 urlpatterns = [
-    path('', include(router.urls))
+    path('auth/login/', MyTokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh-token/', MyTokenRefreshView.as_view(), name='refresh-token'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('', include(router.urls)),
 ]
