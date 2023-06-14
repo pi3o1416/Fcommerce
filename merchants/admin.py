b4bd6aea56cc38f45071c26102c9fc118b1f8f57
= <mmonir.m.29@gmail.com>
2023-06-14 16:22:40 +0600

    Overwrite fieldsets and get_fieldsets property of MerchantAdmin.

--- merchants/admin.py
+++ merchants/admin.py
@@ -1,4 +1,5 @@
 
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from .models import Merchant
 
@@ -8,6 +9,22 @@ class MerchantAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'merchant_id', 'integrate_facebook', 'publish_shop', 'is_staff',
                     'is_superuser', 'is_active', 'created_at']
 
-    def add_view(self, *args, **kwargs):
-        self.exclude = ('integrate_facebook', 'publish_shop')
-        return super(MerchantAdmin, self).add_view(*args, **kwargs)
+    fieldsets = (
+        (_("Merchant info"), {"fields": ("name", "merchant_id", "password")}),
+        (_("Shop Management"), {"fields": ("publish_shop", "integrate_facebook")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None,
+            {
+                "classes": ("wide",),
+                "fields": ("name", "merchant_id", "password"),
+            },
+         ),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
