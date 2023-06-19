ddbaa2aa69013ebc0faeb1ee3a53a2f3a06a7a31
= <mmonir.m.29@gmail.com>
2023-06-19 17:47:14 +0600

    Remove offer section from addform due to model change.

--- products/admin.py
+++ products/admin.py
@@ -11,8 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
         (_('Required Fields'), {'fields': ('name', 'description', 'url', 'image_url', 'currency', 'price', 'retailer_id')}),
         (_('Product'), {'fields': ('condition', 'availability', 'brand', 'category', 'color', 'expiration_date',
                                    'additional_image_urls', 'additional_variant_attributes', 'size',
-                                   'short_description', 'product_type', 'origin_country', 'material', 'inventory', 'gender')}),
-        (_('Offer'), {'fields': ('offer_price_amount', 'offer_price_start_date', 'offer_price_end_date')}),
+                                   'short_description', 'product_type', 'origin_country', 'material', 'gender')}),
         (_('Sale'), {'fields': ('sale_price', 'sale_price_start_date', 'sale_price_end_date')}),
         (_('Importer Information'), {'fields': ('importer_name',)}),
         (_('Other'), {'fields': ('pattern', 'return_policy_days', 'visibility', 'start_date')})
