05c46a719272903784918e0735c23fec1f544f6a
= <mmonir.m.29@gmail.com>
2023-06-20 18:20:34 +0600

    Add create method to ProductSerializer.

--- products/serializers.py
+++ products/serializers.py
@@ -1,17 +1,32 @@
 
+from django.db import transaction
+from django_countries.serializers import CountryFieldMixin
 from rest_framework import serializers
+
 from .models import Product, MerchantProducts
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(CountryFieldMixin, serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [field.name for field in Product._meta.fields]
 
+    @transaction.atomic
+    def create(self, validated_data: dict):
+        """
+        Create a new product. use the new product to create a merchant product
+        and return MerchantProducts instance
+        """
+        product = Product.objects.create(**validated_data)
+        merchant = self.context['request'].user
+        MerchantProducts.objects.create(
+            merchant=merchant,
+            product=product
+        )
+        return product
+
 
 class MerchantProductSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-
     class Meta:
-        model = Product
-        field = [field.name for field in MerchantProducts._meta.fields]
+        model = MerchantProducts
+        fields = '__all__'
