2a957d02dae69d43b6472c46c6e5d76167137a1d
= <mmonir.m.29@gmail.com>
2023-06-26 17:47:43 +0600

    Add signals to app ready.

--- facebook_integration/apps.py
+++ facebook_integration/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class FacebookIntegrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'facebook_integration'
+
+    def ready(self):
+        try:
+            from . import signals as _
+        except Exception:
+            pass
