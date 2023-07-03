690ca572ff3036f5256d5f2034f9cb84005646a6
= <mmonir.m.29@gmail.com>
2023-07-03 18:05:00 +0600

    Integrate Celery.

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,2 +1,10 @@
 
 from .base import *
+
+CELERY_TIMEZONE = "Asia/Dhaka"
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_TIME_LIMIT = 60
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_BROKER_URL = 'redis://localhost:6379/'
