83785d1964d9138ec4046bbf1b512a449729303c
= <mmonir.m.29@gmail.com>
2023-07-05 18:17:48 +0600

    Update FacebookAdapter.

--- facebook_integration/utils.py
+++ facebook_integration/utils.py
@@ -1,18 +1,27 @@
 
+from django.forms import model_to_dict
 import requests
 
 from enum import Enum
-from products.models import Product
+from .decorators import timeout, retry_on_connection_error
+from .exceptions import AddCatalogItemFailed, DeleteCatalogItemFailed
 
 
-class URLType(Enum):
-    OWNED_PRODUCT_CATALOGS = 'https://graph.facebook.com/v17.0/{}/owned_product_catalogs'
-    OWNED_PAGES = 'https://graph.facebook.com/v17.0/{}/owned_pages'
-    PRODUCT_CATALOGS_ITEMS = 'https://graph.facebook.com/v17.0/{}/products'
-    CATALOG_ITEM_ADD = 'https://graph.facebook.com/v17.0/{}/products'
-    CATALOG_ITEM_DELETE = 'https://graph.facebook.com/v17.0/{}'
-    CATALOG_ITEM_UPDATE = 'https://graph.facebook.com/v17.0/{}'
-    CATALOG_ITEM_READ = 'https://graph.facebook.com/v17.0/{}'
+class GraphAPIUrls(Enum):
+    OWNED_PRODUCT_CATALOGS = 'https://graph.facebook.com/v17.0/{business_id}/owned_product_catalogs'
+    OWNED_PAGES = 'https://graph.facebook.com/v17.0/{business_id}/owned_pages'
+    PRODUCT_CATALOGS_ITEMS = 'https://graph.facebook.com/v17.0/{catalog_id}/products'
+    CATALOG_ITEM_ADD = 'https://graph.facebook.com/v17.0/{catalog_id}/products'
+    CATALOG_ITEM_DELETE = 'https://graph.facebook.com/v17.0/{product_facebook_id}'
+    CATALOG_ITEM_UPDATE = 'https://graph.facebook.com/v17.0/{product_facebook_id}'
+    CATALOG_ITEM_READ = 'https://graph.facebook.com/v17.0/{product_facebook_id}'
+
+
+PRODUCT_FIELDS = ['name', 'description', 'url', 'image_url', 'currency', 'price', 'retailer_id', 'gtin',
+                  'condition', 'availability', 'brand', 'category', 'color', 'visibility', 'expiration_date',
+                  'additional_image_urls', 'additional_variant_attributes', 'start_date', 'size', 'short_description',
+                  'sale_price', 'sale_price_start_date', 'sale_price_end_date', 'product_type', 'pattern',
+                  'origin_country', 'material', 'importer_name', 'gender']
 
 
 class FacebookAdapter:
@@ -22,21 +31,26 @@ class FacebookAdapter:
         self.catalog_id = catalog_id
         self.page_id = page_id
 
-    def generate_url(self, url_type: URLType, **kwargs):
-        if url_type == URLType.OWNED_PRODUCT_CATALOGS:
-            return URLType.OWNED_PRODUCT_CATALOGS.value.format(self.business_id)
-        elif url_type == URLType.OWNED_PAGES:
-            return URLType.OWNED_PAGES.value.format(self.business_id)
-        elif url_type == URLType.PRODUCT_CATALOGS_ITEMS:
-            return URLType.PRODUCT_CATALOGS_ITEMS.value.format(self.catalog_id)
-        elif url_type == URLType.CATALOG_ITEM_ADD:
-            return URLType.CATALOG_ITEM_ADD.value.format(self.catalog_id)
-        elif url_type == URLType.CATALOG_ITEM_DELETE:
-            return URLType.CATALOG_ITEM_DELETE.value.format(kwargs.get('facebook_id'))
-        elif url_type == URLType.CATALOG_ITEM_UPDATE:
-            return URLType.CATALOG_ITEM_UPDATE.value.format(kwargs.get('facebook_id'))
-        elif url_type == URLType.CATALOG_ITEM_READ:
-            return URLType.CATALOG_ITEM_READ.value.format(kwargs.get('facebook_id'))
+    @classmethod
+    def merchant_facebook_adapter(cls, merchant):
+        facebook_integration_data = merchant.facebook_info.decrypted_data()
+        return cls(**facebook_integration_data)
+
+    def generate_url(self, url_type: GraphAPIUrls, **kwargs):
+        if url_type == GraphAPIUrls.OWNED_PRODUCT_CATALOGS:
+            return GraphAPIUrls.OWNED_PRODUCT_CATALOGS.value.format(business_id=self.business_id)
+        elif url_type == GraphAPIUrls.OWNED_PAGES:
+            return GraphAPIUrls.OWNED_PAGES.value.format(business_id=self.business_id)
+        elif url_type == GraphAPIUrls.PRODUCT_CATALOGS_ITEMS:
+            return GraphAPIUrls.PRODUCT_CATALOGS_ITEMS.value.format(catalog_id=self.catalog_id)
+        elif url_type == GraphAPIUrls.CATALOG_ITEM_ADD:
+            return GraphAPIUrls.CATALOG_ITEM_ADD.value.format(catalog_id=self.catalog_id)
+        elif url_type == GraphAPIUrls.CATALOG_ITEM_DELETE:
+            return GraphAPIUrls.CATALOG_ITEM_DELETE.value.format(product_facebook_id=kwargs.get('facebook_id'))
+        elif url_type == GraphAPIUrls.CATALOG_ITEM_UPDATE:
+            return GraphAPIUrls.CATALOG_ITEM_UPDATE.value.format(product_facebook_id=kwargs.get('facebook_id'))
+        elif url_type == GraphAPIUrls.CATALOG_ITEM_READ:
+            return GraphAPIUrls.CATALOG_ITEM_READ.value.format(product_facebook_id=kwargs.get('facebook_id'))
         return None
 
     def _make_get_request(self, url, query_params=None):
@@ -65,58 +79,96 @@ class FacebookAdapter:
         response = requests.delete(url=url, params=params)
         return response
 
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def get_owned_catalogs(self):
-        url = self.generate_url(URLType.OWNED_PRODUCT_CATALOGS)
+        url = self.generate_url(GraphAPIUrls.OWNED_PRODUCT_CATALOGS)
         response = self._make_get_request(url=url)
         if response.status_code == 200:
             return response.json()
         return None
 
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def get_owned_pages(self):
-        url = self.generate_url(URLType.OWNED_PAGES)
+        url = self.generate_url(GraphAPIUrls.OWNED_PAGES)
         response = self._make_get_request(url=url)
         if response.status_code == 200:
             return response.json()
         return None
 
-    def get_catalog_items(self):
-        product_fields = [field.name for field in Product._meta.fields if field.name not in ['facebook_id']]
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
+    def get_catalog_items(self, query_fields=None):
+        query_fields = PRODUCT_FIELDS if query_fields is None else query_fields
         response = self._make_get_request(
-            url=self.generate_url(URLType.PRODUCT_CATALOGS_ITEMS),
-            query_params={"fields": ','.join(product_fields)}
+            url=self.generate_url(GraphAPIUrls.PRODUCT_CATALOGS_ITEMS),
+            query_params={"fields": ','.join(query_fields)}
         )
         if response.status_code == 200:
-            return response.json()
-        return None
+            return response.json()['data']
+        return {}
 
-    def add_catalog_item(self, data):
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
+    def add_catalog_item(self, product):
         response = self._make_post_request(
-            url=self.generate_url(URLType.CATALOG_ITEM_ADD),
-            data=data
+            url=self.generate_url(GraphAPIUrls.CATALOG_ITEM_ADD),
+            data=product.to_facebook_representation()
         )
         return response
 
+    def bulk_add_catalog_item(self, products):
+        added_products = {}
+        try:
+            for product in products:
+                response = self.add_catalog_item(product=product)
+                if response.status_code == 200:
+                    added_products[product.id] = {'facebook_id': response.json()['id']}
+                else:
+                    raise AddCatalogItemFailed()
+            return added_products
+        except AddCatalogItemFailed:
+            finished = self._rollback_added_products(added_products=added_products)
+            return None
+
+    def _rollback_added_products(self, added_products):
+        added_products_facebook_id = [product['facebook_id'] for product in added_products.values()]
+        for facebook_id in added_products_facebook_id:
+            response = self.remove_catalog_item(facebook_id=facebook_id)
+            if response.status_code != 200:
+                raise DeleteCatalogItemFailed("Catalog Item Delete Failed")
+        return True
+
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def remove_catalog_item(self, facebook_id):
-        url = self.generate_url(url_type=URLType.CATALOG_ITEM_DELETE, facebook_id=facebook_id)
+        url = self.generate_url(url_type=GraphAPIUrls.CATALOG_ITEM_DELETE, facebook_id=facebook_id)
         response = self._make_delete_request(url=url)
         return response
 
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def update_catalog_item(self, facebook_id, data):
-        url = self.generate_url(url_type=URLType.CATALOG_ITEM_UPDATE, facebook_id=facebook_id)
+        url = self.generate_url(url_type=GraphAPIUrls.CATALOG_ITEM_UPDATE, facebook_id=facebook_id)
         response = self._make_put_request(
             url=url,
             data=data
         )
         return response
 
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def get_item_detail(self, facebook_id):
-        url = self.generate_url(url_type=URLType.CATALOG_ITEM_READ, facebook_id=facebook_id)
+        url = self.generate_url(url_type=GraphAPIUrls.CATALOG_ITEM_READ, facebook_id=facebook_id)
         response = self._make_get_request(url=url)
         return response
 
     def get_page_detail(self):
         pass
 
+    @retry_on_connection_error(number_of_retry=5)
+    @timeout(seconds=5)
     def verify_info(self):
         owned_pages = self.get_owned_pages()
         owned_catalogs = self.get_owned_catalogs()
