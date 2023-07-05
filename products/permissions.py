af80c0d7d762638d23574cef5e40da9f9af0a9c1
= <mmonir.m.29@gmail.com>
2023-07-05 14:57:25 +0600

    Update IsProductOwner due to model change.

--- products/permissions.py
+++ products/permissions.py
@@ -1,11 +1,11 @@
 
 from rest_framework.permissions import BasePermission
-from .models import Product
+from .models import MerchantProduct
 
 
 class IsProductOwner(BasePermission):
-    def has_object_permission(self, request, view, product: Product):
-        product_owner_id = product.product_owner.merchant_id
+    def has_object_permission(self, request, view, merchant_product: MerchantProduct):
+        product_owner_id = merchant_product.merchant_id
         if product_owner_id == request.user.id:
             return True
         return False
