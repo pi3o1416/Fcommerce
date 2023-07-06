2ae4f84326cd0a603564fbfa991e6bcd4f866fa8
= <mmonir.m.29@gmail.com>
2023-07-06 18:32:39 +0600

    Update AddCatalogItemFailed and add SyncBetweenMerchantAndFacebookFailed.

--- facebook_integration/exceptions.py
+++ facebook_integration/exceptions.py
@@ -1,7 +1,15 @@
 
 
 class AddCatalogItemFailed(Exception):
-    pass
+    def __init__(self, response, *args, **kwargs):
+        self.response = response
+        super().__init__(*args, **kwargs)
+
+
+class SyncBetweenMerchantAndFacebookFailed(Exception):
+    def __init__(self, response, *args, **kwargs):
+        self.response = response
+        super().__init__(*args, **kwargs)
 
 
 class DeleteCatalogItemFailed(Exception):
