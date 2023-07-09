eeaaf7772edff34fb2db7a8d6c2a649aa425ff2e
= <mmonir.m.29@gmail.com>
2023-07-09 18:29:24 +0600

    Add log_api_errors decorator.

--- facebook_integration/decorators.py
+++ facebook_integration/decorators.py
@@ -1,7 +1,9 @@
 
+import logging
 from concurrent import futures
 import time
 import functools
+from .exceptions import FacebookAPIErrorException
 
 
 def retry_on_connection_error(number_of_retry):
@@ -32,3 +34,23 @@ def timeout(seconds):
             return result
         return wrapper
     return decorator
+
+
+def log_api_errors(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger('fcommerce')
+        # Retrieve Merchant from FacebookAdapter Instance args[0] refer to self
+        merchant = args[0].merchant
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except FacebookAPIErrorException as exception:
+            logger.error({
+                "function": func.__name__,
+                "message": exception.message,
+                "response": exception.response.json() if exception.response is not None else None,
+                "status_code": exception.response.status_code if exception.response is not None else None,
+                "merchant_id": merchant.merchant_id
+            })
+    return wrapper
