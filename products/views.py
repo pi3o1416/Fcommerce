6de25b89096ef8e77cc5663619dfeda461500929
= <mmonir.m.29@gmail.com>
2023-07-10 14:59:22 +0600

    Modify CRUD views method and add sync_inventory_with_facebook.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 
 from django.http import Http404
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 from services.paginations import CustomPageNumberPagination
 from services.utils import customize_response, exception_handler
@@ -10,6 +13,7 @@ from services.constants import ErrorTypes
 from .permissions import IsProductOwner
 from .models import MerchantProduct
 from .serializers import MerchantProductSerializer
+from .tasks import add_product_on_facebook, delete_product, sync_inventory_with_facebook, update_product_on_facebook
 
 
 class MerchantProductsViewSet(ModelViewSet):
@@ -28,6 +32,11 @@ class MerchantProductsViewSet(ModelViewSet):
                 error_type=ErrorTypes.FORM_FIELD_ERROR.value
             )
 
+    def perform_create(self, serializer):
+        # Create Product on facebook inventory
+        product = serializer.save()
+        add_product_on_facebook.delay(product_id=product.id)
+
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         return customize_response(response=response, custom_message='Merchant Product list')
@@ -43,9 +52,10 @@ class MerchantProductsViewSet(ModelViewSet):
                 error_type=ErrorTypes.OBJECT_DOES_NOT_EXIST.value
             )
 
-    def update(self, request, *args, **kwargs):
+    def update(self, request, id, *args, **kwargs):
         try:
             response = super().update(request, *args, **kwargs)
+            # Update on facebook inventory
             return customize_response(response=response, custom_message='Merchant Product Update Successful')
         except Http404 as excpt:
             return exception_handler(
@@ -60,8 +70,13 @@ class MerchantProductsViewSet(ModelViewSet):
                 error_type=ErrorTypes.FORM_FIELD_ERROR.value
             )
 
-    def destroy(self, request, *args, **kwargs):
+    def perform_update(self, serializer):
+        product = serializer.save()
+        update_product_on_facebook.delay(product_id=product.id)
+
+    def destroy(self, request, id, *args, **kwargs):
         try:
+            # Delete from facebook inventory
             response = super().destroy(request, *args, **kwargs)
             return customize_response(response=response, custom_message='Merchant Product Delete Successful')
         except Http404 as excpt:
@@ -70,6 +85,16 @@ class MerchantProductsViewSet(ModelViewSet):
                 message='Merchant Product Delete Failed',
                 error_type=ErrorTypes.OBJECT_DOES_NOT_EXIST.value
             )
+
+    def perform_destroy(self, instance: MerchantProduct):
+        delete_product.delay(product_id=instance.id)
+
+    @action(methods=['get'], detail=False, url_path='sync-with-facebook')
+    def sync_inventory_with_facebook(self, request):
+        merchant = request.user
+        sync_inventory_with_facebook.delay(merchant_id=merchant.id)
+        response = Response(data={'detail': 'Request Accepted'}, status=status.HTTP_202_ACCEPTED)
+        return customize_response(response, 'A Sync process is in Progress')
 
     def get_permissions(self):
         permissions = [IsAuthenticated]
