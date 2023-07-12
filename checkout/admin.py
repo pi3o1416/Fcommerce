d3058868245c5022de01b28612cb246148656133
= <mmonir.m.29@gmail.com>
2023-07-12 15:42:35 +0600

    Add OrderAdmin for Order model.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'merchant', 'customer_name', 'customer_email', 'customer_phone_no', 'shipping_address',
+                    'created_at', 'payment_currency', 'total_amount', 'payment_status']
+    readonly_fields = ['id', 'merchant', 'customer_name', 'customer_email', 'customer_phone_no', 'shipping_address', 'products']
