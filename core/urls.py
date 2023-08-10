fb6482230bc1cc916d0bc687de81542fbab45728
= <mmonir.m.29@gmail.com>
2023-08-10 14:09:14 +0600

    Add static and media urls.

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -27,5 +29,7 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
     path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger",),
-
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
